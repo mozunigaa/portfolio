db39e178fb70b2958c674ef1469f92402a23591d
mozuniga93 <mzunigaa@ucenfotec.ac.cr>
2022-03-10 14:15:21 -0600

    dev

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-      <Router basename={window.location.pathname || ''}>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>  
           <Route path="/" component= {Home} exact />
         </Switch>

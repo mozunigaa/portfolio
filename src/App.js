ae8538369f873f0e499e27fcf74aaf37376a8d11
mozuniga93 <mzunigaa@ucenfotec.ac.cr>
2022-03-10 14:07:32 -0600

    deploy

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
-      <Switch>  
-        <Route path="/" component= {Home} exact />
-      </Switch>
-    </Router>
+    <BrowserRouter basename={window.location.pathname || ''}>
+      <Router>
+        <Switch>  
+          <Route path="/" component= {Home} exact />
+        </Switch>
+      </Router>
+    </BrowserRouter>
   );
 }
 
